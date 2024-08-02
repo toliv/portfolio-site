f09796d54406157a7bc73f12eb6f04cb0793094d
Tony Oliverio <tony.oliverio1@gmail.com>
2024-08-02 12:09:19 -0400

    .

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,6 @@ export default function Page() {
       <p className="mb-4">
         {`Full stack engineer with a passion for creative engineering solutions, intuitive product experiences, and happy customers 🙂`}
       </p>
-      <p className="mb-4">
-        {`Welcome to my personal site where I've collected some technical writing on some of my projects and experiences.`}
-      </p>
       <div className="my-8 text-2xl">{`Projects`}</div>
       <div className="my-8">
         <ProjectPosts />
